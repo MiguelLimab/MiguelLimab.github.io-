1e5941e983e49249fdcd968c249950ab1b64ad42
MiguelLimab <103779001+MiguelLimab@users.noreply.github.com>
2022-07-03 21:21:12 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const btns = document.querySelectorAll(".question-btn");
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const quention = e.currentTarget.parentElement.parentElement;
-    quention.classList.toggle("show-text");
-    });
-  });
